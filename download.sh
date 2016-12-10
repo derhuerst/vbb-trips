341f0e6dd5d1d947d8420e73615484cc9efe0e85
Jannis R <mail@jannisr.de>
2016-12-10 18:19:12 +0100

    tar -> unzip

--- download.sh
+++ download.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+URL='https://github.com/derhuerst/vbb-gtfs/archive/master.zip'
+
 if [[ -ne $GITHUB_TOKEN ]]; then
 	echo 'Downloading vbb-gtfs using access token.'
-	curl -# -H "Authorization: token $GITHUB_TOKEN" -L 'https://github.com/derhuerst/vbb-gtfs/archive/master.zip' | tar -xf- --strip-components 1 -C build/data/
+	curl -# -H "Authorization: token $GITHUB_TOKEN" -L $URL > data.zip
 else
 	echo 'Downloading vbb-gtfs without access token.'
-	curl -# -L 'https://github.com/derhuerst/vbb-gtfs/archive/master.zip' | tar -xf- --strip-components 1 -C build/data/
+	curl -# -L $URL > data.zip
 fi
+
+unzip -ju data.zip -d build/data
+rm data.zip
