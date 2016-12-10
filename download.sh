f1bb387ff43ce3a7f3379193e3108596dcbc8679
Jannis R <mail@jannisr.de>
2016-12-10 17:59:16 +0100

    Revert "fix syntax error in download script"
    This reverts commit 336abcfb30e686edfe7c9cca1485634a871177bf.

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -ne $GITHUB_TOKEN ]; then
+if [[ -ne $GITHUB_TOKEN ]]; then
 	echo 'Downloading vbb-gtfs using access token.'
 	curl -# -H "Authorization: token $GITHUB_TOKEN" -L 'https://github.com/derhuerst/vbb-gtfs/archive/master.zip' | tar -xf- --strip-components 1 -C build/data/
 else
