4f194311caf1ff72e2802cd38a1d748ba05ef243
Jannis R <mail@jannisr.de>
2016-12-21 23:14:18 +0100

    update build, 1.1.0

--- download.sh
+++ download.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
-URL='https://github.com/derhuerst/vbb-gtfs/archive/master.zip'
-
-if [[ -ne $GITHUB_TOKEN ]]; then
-	echo 'Downloading vbb-gtfs using access token.'
-	curl -# -H "Authorization: token $GITHUB_TOKEN" -L $URL > data.zip
-else
-	echo 'Downloading vbb-gtfs without access token.'
-	curl -# -L $URL > data.zip
-fi
-
-unzip -ju data.zip -d build/data
-rm data.zip
+curl -L --compressed https://vbb-gtfs.jannisr.de/latest/calendar.txt > build/data/calendar.txt
+curl -L --compressed https://vbb-gtfs.jannisr.de/latest/calendar_dates.txt > build/data/calendar_dates.txt
+curl -L --compressed https://vbb-gtfs.jannisr.de/latest/routes.txt > build/data/routes.txt
+curl -L --compressed https://vbb-gtfs.jannisr.de/latest/stop_times.txt > build/data/stop_times.txt
+curl -L --compressed https://vbb-gtfs.jannisr.de/latest/trips.txt > build/data/trips.txt
