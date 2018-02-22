2e30bf0e6d615ce6c20262d72db836d44e2c3383
Jannis R <mail@jannisr.de>
2018-02-22 16:34:37 +0100

    rewrite build, part 1

---
+++ build/download.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd $(dirname $0)
+
+gzip='accept-encoding: gzip'
+base_url='https://vbb-gtfs.jannisr.de/latest/'
+download () {
+	wget -nc -qc --show-progress --header=$gzip -O data/$1 $base_url$1
+}
+
+download 'agency.txt'
+download 'calendar.txt'
+download 'calendar_dates.txt'
+download 'routes.txt'
+download 'stop_times.txt'
+download 'trips.txt'
--- download.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -L --compressed https://vbb-gtfs.jannisr.de/latest/calendar.txt > build/data/calendar.txt
-curl -L --compressed https://vbb-gtfs.jannisr.de/latest/calendar_dates.txt > build/data/calendar_dates.txt
-curl -L --compressed https://vbb-gtfs.jannisr.de/latest/routes.txt > build/data/routes.txt
-curl -L --compressed https://vbb-gtfs.jannisr.de/latest/stop_times.txt > build/data/stop_times.txt
-curl -L --compressed https://vbb-gtfs.jannisr.de/latest/trips.txt > build/data/trips.txt
