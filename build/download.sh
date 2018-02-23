dc48b1e54772ab2018f3f3b5c8748aba26f4f961
Jannis R <mail@jannisr.de>
2018-02-23 16:28:15 +0100

    debug wget 💚

--- build/download.sh
+++ build/download.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)/data
 
 base_url='https://vbb-gtfs.jannisr.de/latest/'
 download () {
-	wget -q -N -c --header='accept-encoding: gzip' $base_url$1
+	wget -N -c --header='accept-encoding: gzip' $base_url$1
 }
 
 download 'agency.txt'
