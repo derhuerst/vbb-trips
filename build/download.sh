c1c5777e65f6d65eb8cc03ab945e93dd29bf8ab1
Jannis R <mail@jannisr.de>
2019-10-14 11:45:32 +0200

    rebuild with latest data, 3.11.0

--- build/download.sh
+++ build/download.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)/data
 
 base_url='https://vbb-gtfs.jannisr.de/latest/'
 download () {
-	curl -s -z --compressed $base_url$1 >$1
+	curl -s -z -L --compressed $base_url$1 >$1
 }
 
 download 'agency.csv'
