345f7eaa60ed47ec8ff981449fd96bad187c3837
Jannis R <mail@jannisr.de>
2020-04-18 16:34:53 +0200

    download script: cache using ETags
    [ci skip]

--- build/download.sh
+++ build/download.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+set -e
+
 cd $(dirname $0)/data
 
 base_url='https://vbb-gtfs.jannisr.de/latest/'
 download () {
-	curl -s -z -L --compressed $base_url$1 >$1
+	curl -z -L --compressed --etag-compare "$1.etag" --etag-save "$1.etag" $base_url$1 -o $1
 }
 
 download 'agency.csv'
